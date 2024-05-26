56d005a6524939a049268aceff9ff0c7b767ce8d
TiagoPeSouzaa <tiago.souza@sptech.school>
2024-05-26 12:08:34 -0300

    Atualizações CTA,cores e finalizando

--- public/js/testimonials.js
+++ public/js/testimonials.js
@@ -4,10 +4,10 @@ const feedbackImages = document.querySelectorAll('.feedback-avatar');
 // Adiciona um evento de clique para cada imagem
 feedbackImages.forEach(image => {
     image.addEventListener('click', () => {
-        // Clona a imagem clicada e adiciona ao card
-        const clonedImage = image.cloneNode(true);
-        document.getElementById('card').innerHTML = ''; // Limpa o conteúdo anterior do card
-        document.getElementById('card').appendChild(clonedImage);
+        // // Clona a imagem clicada e adiciona ao card
+        // const clonedImage = image.cloneNode(true);
+        // document.getElementById('card').innerHTML = ''; // Limpa o conteúdo anterior do card
+        // document.getElementById('card').appendChild(clonedImage);
 
         // Exibe a sobreposição e o card
         document.getElementById('overlay').style.display = 'block';
