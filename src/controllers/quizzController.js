766975a894648fbf3be1aeabf46f15f55b0e8b21
TiagoPeSouzaa <tiago.souza@sptech.school>
2024-06-06 10:11:32 -0300

    Site pronto

--- src/controllers/quizzController.js
+++ src/controllers/quizzController.js
@@ -2,24 +2,18 @@ var quizzModel = require("../models/quizzModel");
 
 function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
-   
+
     var score = req.body.scoreServer;
     var usuarioId = req.body.usuarioIdServer;
     // var empresaId = req.body.empresaServer;
     // var cpf = req.body.cpfServer;
-     
+
 
     // Faça as validações dos valores
     if (score == undefined) {
         res.status(400).send("Seu score está undefined!");
     } else if (usuarioId == undefined) {
         res.status(400).send("Seu usuarioId está undefined!");
-    // } else if (senha == undefined) {
-    //     res.status(400).send("Sua senha está undefined!");
-    // } else if (empresaId == undefined) {
-    //     res.status(400).send("Sua empresa está undefined!");
-    // } else if (cpf == undefined) {
-    //     res.status(400).send("Seu cpf está undefined!");
     } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
@@ -42,6 +36,5 @@ function cadastrar(req, res) {
 }
 
 module.exports = {
-    // autenticar,
     cadastrar
 }
--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -6,26 +6,35 @@
 comandos para mysql server
 */
 
-CREATE DATABASE aquatech;
+CREATE DATABASE tsound;
 
-USE aquatech;
+drop database tsound;
 
-CREATE TABLE empresa (
+USE tsound;
+
+/*CREATE TABLE empresa (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	razao_social VARCHAR(50),
 	cnpj CHAR(14)
-);
+);*/
 
-CREATE TABLE usuario (
+CREATE TABLE tsound.usuario (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(50),
 	email VARCHAR(50),
-	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+	senha VARCHAR(50)
+	/*fk_empresa INT,
+	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)*/
 );
 
-CREATE TABLE aviso (
+create table tsound.quizz (
+id int primary key auto_increment,
+score int,
+fkUsuario int,
+constraint fkUsuario foreign key (fkUsuario) references usuario (id)
+);
+
+CREATE TABLE tsound.aviso (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	titulo VARCHAR(100),
 	descricao VARCHAR(150),
@@ -33,27 +42,88 @@ CREATE TABLE aviso (
 	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
 );
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
-);
+-- select das tabelas
+select * from usuario;
+select * from tsound.quizz;
+select * from tsound.aviso;
 
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
+insert into tsound.usuario (nome, email, senha)
+values ('lucas', 'lucas.souza@hotmail.com', '123456');
 
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
+-- media de score agrupados por nome
+select u.nome 'Nome' ,
+round(avg(q.score))
+from tsound.quizz as q inner join tsound.usuario as u on q.fkUsuario = u.id
+group by u.nome;
 
-insert into empresa (razao_social, cnpj) values ('Empresa 1', '00000000000000');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
+-- media geral
+select 
+round(avg(q.score))
+from tsound.quizz as q ;
+
+select count(u.nome) 'Quantidade de pessoas'
+from tsound.usuario as u;
+select 
+    round(avg(q.score)) 'Quantidade'
+    from tsound.quizz as q;
+
+	
+-- -- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
+-- -- Você precisa executar os comandos no banco de dados para criar as tabelas,
+-- -- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+
+-- /*
+-- comandos para mysql server
+-- */
+
+-- CREATE DATABASE aquatech;
+
+-- USE aquatech;
+
+-- CREATE TABLE empresa (
+-- 	id INT PRIMARY KEY AUTO_INCREMENT,
+-- 	razao_social VARCHAR(50),
+-- 	cnpj CHAR(14)
+-- );
+
+-- CREATE TABLE usuario (
+-- 	id INT PRIMARY KEY AUTO_INCREMENT,
+-- 	nome VARCHAR(50),
+-- 	email VARCHAR(50),
+-- 	senha VARCHAR(50),
+-- 	fk_empresa INT,
+-- 	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+-- );
+
+-- CREATE TABLE aviso (
+-- 	id INT PRIMARY KEY AUTO_INCREMENT,
+-- 	titulo VARCHAR(100),
+-- 	descricao VARCHAR(150),
+-- 	fk_usuario INT,
+-- 	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+-- );
+
+-- create table aquario (
+-- /* em nossa regra de negócio, um aquario tem apenas um sensor */
+-- 	id INT PRIMARY KEY AUTO_INCREMENT,
+-- 	descricao VARCHAR(300),
+-- 	fk_empresa INT,
+-- 	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+-- );
+
+-- /* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
+
+-- create table medida (
+-- 	id INT PRIMARY KEY AUTO_INCREMENT,
+-- 	dht11_umidade DECIMAL,
+-- 	dht11_temperatura DECIMAL,
+-- 	luminosidade DECIMAL,
+-- 	lm35_temperatura DECIMAL,
+-- 	chave TINYINT,
+-- 	momento DATETIME,
+-- 	fk_aquario INT,
+-- 	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
+-- );
+
+-- insert into empresa (razao_social, cnpj) values ('Empresa 1', '00000000000000');
+-- insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
